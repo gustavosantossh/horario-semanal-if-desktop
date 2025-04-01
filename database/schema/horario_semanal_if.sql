2aaac723ac7e4196007f71637bc85e797b52bb06
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2025-03-31 21:35:45 -0300

    first commit

---
+++ database/schema/horario_semanal_if.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA IF NOT EXISTS horario_semanal_if;
+
+use horario_semanal_if;
+
+CREATE TABLE IF NOT EXISTS horarios (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome_da_turma VARCHAR(255),
+	curso VARCHAR(255),
+	disciplina VARCHAR(255),
+	dia_da_semana VARCHAR(255),
+	horario_da_aula VARCHAR(255),
+	sala VARCHAR(255),
+	professor VARCHAR(255)
+);
