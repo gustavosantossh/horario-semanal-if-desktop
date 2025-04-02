aca4e22a000b9f2204170abfa6879a8c19a9f835
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2025-04-02 00:52:09 -0300

    feat: add label and entry

--- main.py
+++ main.py
@@ -21,6 +21,16 @@ class App:
         
         loginFrame.place(relx=0.5, rely=0.5, anchor="center" , relheight=0.5, relwidth=0.4)
         
-        loginTextLabel = Label(loginFrame, text="aaaaaaaaaa")
+        # ---------------------------
+        
+        loginTextLabel = Label(loginFrame, text="Email").place(relx=0.4, rely=0.2, anchor="e")
+        
+        loginInputEmail = Entry(loginFrame, ).place(relx=0.5, rely=0.3, anchor="center")
+        
+        #  ----------------------------
+        
+        loginTextLabelSenha = Label(loginFrame, text="Senha").place(relx=0.4, rely=0.5, anchor="e")
+        
+        loginInputSenha = Entry(loginFrame, ).place(relx=0.5, rely=0.6, anchor="center")
         
 App()
