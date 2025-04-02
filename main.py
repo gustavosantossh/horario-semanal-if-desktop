97bd7109c181c9afcc689c98810a61d26cec66c2
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2025-04-02 00:10:02 -0300

    feat: add methods login and config

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from tkinter import *
+
+root = Tk()
+
 class App:
     def __init__(self):
         self.window()
     
     def window(self):
-        root = Tk()
+        self.config()
+        self.login()
         root.mainloop()
+        
+    def config(self):
+        root.config(background="#04030F")
+        root.minsize(800, 500)
+        root.title("Horário Semanal")
+        
+    def login(self):
+        loginFrame = Frame(root, bd=4)
+        
+        loginFrame.place(relx=0.5, rely=0.5, anchor="center" , relheight=0.5, relwidth=0.4)
+        
+        loginTextLabel = Label(loginFrame, text="aaaaaaaaaa")
         
 App()
